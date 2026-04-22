a20294d560cd631866914f801f90e772e3d61a23
FrancescoPaoloL <francescopaololezza@gmail.com>
2026-04-22 20:02:17 +0200

    feat: add config

---
+++ config.py
@@ -0,0 +1,8 @@
+# ref: arXiv:2603.09078 (XSA, Apple 2026)
+
+MODELS   = ["gpt2", "gpt2-medium", "gpt2-large"]
+N_SEQS   = 32
+SEQ_LEN  = 128
+SEED     = 42
+PLOT_OUT = "attention_similarity_bias.png"
+
